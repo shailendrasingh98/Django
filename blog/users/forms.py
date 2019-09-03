ce50afe6a567e129b23bd49e47394ae4665ad94f
shailendra_singh <dshailendrasingh98@gmail.com>
2019-09-04 00:56:28 +0530

    update profile

--- blog/users/forms.py
+++ blog/users/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
+from .models import Profile
 
 class UserRegisterForm(UserCreationForm):
     """Create custum user registeration form """
@@ -10,3 +11,16 @@ class UserRegisterForm(UserCreationForm):
         model = User
         fields = ['username', 'email',\
                 'password1', 'password2']
+
+class UserUpdateForm(forms.ModelForm):
+    """docstring for UserUpdateForm."""
+    email = forms.EmailField()
+    class Meta:
+        model = User
+        fields = ['username', 'email']
+
+class ProfileUpdateForm(forms.ModelForm):
+    """docstring for PrfileUpdateForm."""
+    class Meta:
+        model = Profile
+        fields = ['image']
